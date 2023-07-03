88749e09c9f5fad0c407919312b6d82ae510b39a
parag koche <paragkoche0@gmail.com>
2023-07-04 01:35:39 +0530

    blog

---
+++ src/supabase/index.ts
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js"
+
+export default createClient('https://rqumerxnwmqfikvpjbph.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdW1lcnhud21xZmlrdnBqYnBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0MTM1MTMsImV4cCI6MjAwMzk4OTUxM30.bAYd8sIbl6SbdYdP-YeFg5ut_k3quGryPPzH9Msc7Z0')
